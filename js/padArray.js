d1458576b122cc8560bc82ee6261949453dfc712
KevinBaoB <kevinbaobui@gmail.com>
2022-05-28 14:46:36 -0500

    First

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,18 @@
 // REMEMBER TO PSEUDOCODE
-const pad = (array, minSize, value=null) => {
+const pad = (array, minSize, value = null) => {
+  if (array.length === minSize || minSize === 0) {
+    return array;
+  }
+  for (let i = 0; i <= minSize - array.length; i++) {
+    if (value === null) {
+      array.push("None");
+    } else {
+      array.push(value);
+    }
+  }
+  return array;
+};
 
-}
+console.log(pad([1, 2, 3], 5));
+console.log(pad([1, 2, 3], 5, "apple"));
+console.log(pad([1, 2, 3], 3));
